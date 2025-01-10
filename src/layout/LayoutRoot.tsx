8fd8fe1d2ee431762a48ba8a4c9efb542399eb61
Alina Geesen-Stucky <alina.geesen@xitaso.com>
2025-01-10 11:41:47 +0100

    feat(mnestix-related-infos-footer): created footer with copyright, imprint and data privacy (WIP)

--- src/layout/LayoutRoot.tsx
+++ src/layout/LayoutRoot.tsx
@@ -1,8 +1,16 @@
 'use client';
-import { Box, styled } from '@mui/material';
+import { Box, styled, Typography } from '@mui/material';
 import { NotificationOutlet } from 'components/basics/NotificationOutlet';
 import { Header } from './Header';
-import { ReactNode } from 'react';
+import React, { ReactNode } from 'react';
+import { BottomNavigation } from '@mui/material';
+import { BottomNavigationAction } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import Link from '@mui/material/Link';
+import { useEnv } from 'app/env/provider';
+import { ExternalLink } from 'layout/menu/ExternalLink';
+import { messages } from 'lib/i18n/localization';
+import { FormattedMessage } from 'react-intl';
 
 const StyledBox = styled(Box)(() => ({
         // toolbar min-height is 56px
@@ -18,6 +26,11 @@ type Props = {
 };
 
 export function LayoutRoot({ children }: Props) {
+    const env = useEnv();
+    const imprintString = env.IMPRINT_URL;
+    const dataPrivacyString = env.DATA_PRIVACY_URL;
+    const copyrightString = `Copyright © ${new Date().getFullYear()} XITASO GmbH`;
+
     return (
         <Box display="flex" height="100%" flexDirection="column">
             <Box display="flex" flex={1} flexDirection="column">
@@ -25,6 +38,37 @@ export function LayoutRoot({ children }: Props) {
                     <Header />
                     {children}
                 </StyledBox>
+                <Paper  elevation={3}>
+                <BottomNavigation sx={{ bottom: 0, left: 0, right: 0 , position: 'fixed', backgroundColor: 'transparent' }}>
+                    <Typography color="text.secondary" fontSize="small" sx={{ margin: '15px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                        {copyrightString}
+                    </Typography>
+
+                    <Typography fontSize="small" sx={{ display: 'flex', maxWidth: '150px', alignItems: 'center', justifyContent: 'center' }}>
+                        <Link
+                            href={dataPrivacyString}
+                            target="_blank"
+                        >
+                            <FormattedMessage {...messages.mnestix.dataPrivacy} />
+                        </Link>
+                    </Typography>
+
+                    <Typography color="text.secondary" fontSize="small" sx={{ margin: '7px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                      |
+                    </Typography>
+
+                    <Typography  fontSize="small" sx={{ display: 'flex', maxWidth: '150px', alignItems: 'center', justifyContent: 'center' }}>
+                        <Link
+                            href={imprintString}
+                            target="_blank"
+                            sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                        >
+                            <FormattedMessage {...messages.mnestix.imprint} />
+                        </Link>
+                    </Typography>
+
+                </BottomNavigation>
+                </Paper>
                 <NotificationOutlet />
             </Box>
         </Box>
