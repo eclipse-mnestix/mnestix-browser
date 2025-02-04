6209a7ae93b5e7e5916d8c25f066ebe23b6da267
Nils Rothamel <nils.rothamel@xitaso.com>
2025-02-04 15:36:18 +0100

    Add translation to the error tooltip messages

--- src/components/basics/VerticalTabSelector.tsx
+++ src/components/basics/VerticalTabSelector.tsx
@@ -66,9 +66,9 @@ export function VerticalTabSelector(props: VerticalTabSelectorProps) {
     const selectedCSSClass = (id: string) => (id === props.selected?.id ? 'selected' : '');
 
     const handleSubmodelInfoModalClose = () => {
-        setHoveredItem(undefined)
+        setHoveredItem(undefined);
         setSubmodelInfoDialogOpen(false);
-    }
+    };
 
     const getTranslationKey = (errorCode: string) => {
         switch (errorCode) {
@@ -84,49 +84,59 @@ export function VerticalTabSelector(props: VerticalTabSelectorProps) {
     };
 
     return (
-        <Box sx={{ 'Button:nth-of-type(1)': { borderColor: 'transparent' } }}
-             onMouseLeave={() => setHoveredItem(undefined)}
+        <Box
+            sx={{ 'Button:nth-of-type(1)': { borderColor: 'transparent' } }}
+            onMouseLeave={() => setHoveredItem(undefined)}
         >
             {props.items.map((item, index) => {
                 return (
-                    <Box
-                        key={index}
-                        onMouseEnter={() => setHoveredItem(item)}>
-                    <Tab
-                        data-testid="submodel-tab"
-                        onClick={() => props.setSelected && props.setSelected(item)}
-                        className={`tab-item ${selectedCSSClass(item.id)}`}
-                        disabled={!!item.submodelError}
-                    >
-                        <Box display="flex" alignItems="left" style={{ whiteSpace: 'nowrap', paddingRight: '20px' }}>
-                            <Typography>{tooltipText(item.label, 40) || ''}</Typography>
-                        </Box>
-
-                        <Box display="flex" alignItems="center" gap={2} >
-                            <Box visibility={ (item.id === props.selected?.id) || (item.id === hoveredItem?.id) ? 'visible' : 'hidden'}>
-                                {item.submodelError ? (
-                                    <Tooltip title={t(getTranslationKey(item.submodelError?.toString()))}>
-                                        <Box display="flex"
-                                             sx={{ cursor: 'pointer' }}>
-                                            {item.startIcon}
-                                        </Box>
-                                    </Tooltip>
-                                ) : (
-                                    <Tooltip title={item.id.toString()}>
-                                        <Box display="flex"
-                                             sx={{ cursor: 'pointer' }}
-                                             onClick={(event) => {
-                                                 setSubmodelInfoDialogOpen(true);
-                                                 event.stopPropagation(); // don't open the tab
-                                             }}>
-                                                {item.startIcon}
-                                        </Box>
-                                    </Tooltip>
-                                )}
+                    <Box key={index} onMouseEnter={() => setHoveredItem(item)}>
+                        <Tab
+                            data-testid="submodel-tab"
+                            onClick={() => props.setSelected && props.setSelected(item)}
+                            className={`tab-item ${selectedCSSClass(item.id)}`}
+                            disabled={!!item.submodelError}
+                        >
+                            <Box
+                                display="flex"
+                                alignItems="left"
+                                style={{ whiteSpace: 'nowrap', paddingRight: '20px' }}
+                            >
+                                <Typography>{tooltipText(item.label, 40) || ''}</Typography>
                             </Box>
-                            <ArrowForward color={item.submodelError ? 'disabled' : 'primary'} />
-                        </Box>
-                    </Tab>
+
+                            <Box display="flex" alignItems="center" gap={2}>
+                                <Box
+                                    visibility={
+                                        item.id === props.selected?.id || item.id === hoveredItem?.id
+                                            ? 'visible'
+                                            : 'hidden'
+                                    }
+                                >
+                                    {item.submodelError ? (
+                                        <Tooltip title={t(getTranslationKey(item.submodelError.toString()))}>
+                                            <Box display="flex" sx={{ cursor: 'pointer' }}>
+                                                {item.startIcon}
+                                            </Box>
+                                        </Tooltip>
+                                    ) : (
+                                        <Tooltip title={item.id.toString()}>
+                                            <Box
+                                                display="flex"
+                                                sx={{ cursor: 'pointer' }}
+                                                onClick={(event) => {
+                                                    setSubmodelInfoDialogOpen(true);
+                                                    event.stopPropagation(); // don't open the tab
+                                                }}
+                                            >
+                                                {item.startIcon}
+                                            </Box>
+                                        </Tooltip>
+                                    )}
+                                </Box>
+                                <ArrowForward color={item.submodelError ? 'disabled' : 'primary'} />
+                            </Box>
+                        </Tab>
                     </Box>
                 );
             })}
@@ -137,6 +147,5 @@ export function VerticalTabSelector(props: VerticalTabSelectorProps) {
                 idShort={hoveredItem?.submodelData?.idShort}
             />
         </Box>
-
     );
 }
