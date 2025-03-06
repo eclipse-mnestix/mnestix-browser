d74a4e093789aec2dc5dca188f3036d14bb6cd48
XITASO\pawel.baran <pawel.baran@xitaso.com>
2025-03-06 15:37:47 +0100

    dynamic-rbac-working-solution-init

--- docker-compose/dynamic-rbac/entrypoint.sh
+++ docker-compose/dynamic-rbac/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Sending POST request to the service..."
-curl -v POST http://submodel-repository:8081/submodels \
+curl -v POST http://security-submodel:8081/submodels \
     -H "Content-Type: application/json" \
     -d "$(cat initial-submodel.json)"
