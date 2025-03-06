550f63432e56d13b241f6b9524ddc4f1120bc12e
melanie.gail <melanie.gail@xitaso.com>
2025-03-06 15:30:14 +0100

    dummy data

--- src/app/[locale]/settings/_components/role-settings/RoleSettings.tsx
+++ src/app/[locale]/settings/_components/role-settings/RoleSettings.tsx
@@ -1,6 +1,22 @@
 import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
 import { useTranslations } from 'next-intl';
 
+export type RbacDto = {
+    name: string;
+    action: rbacAction[];
+    type: string;
+    aasIds: string[];
+    submodelIds: string[];
+};
+
+enum rbacAction {
+    CREATE,
+    READ,
+    UPDATE,
+    DELETE,
+    EXECUTE,
+}
+
 export const RoleSettings = () => {
     const t = useTranslations('settings');
 
@@ -10,7 +26,23 @@ export const RoleSettings = () => {
         { label: t('roles.tableHeader.type') },
         { label: t('roles.tableHeader.aasIds') },
         { label: t('roles.tableHeader.submodelIds') },
-        '',
+    ];
+
+    const dummyData: RbacDto[] = [
+        {
+            name: 'basyx-reader-serialization-two',
+            action: [rbacAction.DELETE, rbacAction.CREATE],
+            type: 'aas-environment',
+            aasIds: ['https://aas2.uni-h.de/lni0729', 'https://aas2.uni-h.de/lni0729', 'https://aas2.uni-h.de/lni0729'],
+            submodelIds: ['submodel1', 'submodel2', 'submodel3', 'submodel4', 'submodel5', 'submodel6'],
+        },
+        {
+            name: 'basyx-reader',
+            action: [rbacAction.DELETE, rbacAction.CREATE, rbacAction.EXECUTE],
+            type: 'aas-repository',
+            aasIds: ['https://aas2.uni-h.de/lni0729', 'https://aas2.uni-h.de/lni0729'],
+            submodelIds: ['submodel1', 'submodel2', 'submodel3', 'submodel6'],
+        },
     ];
 
     return (
@@ -31,26 +63,20 @@ export const RoleSettings = () => {
                             ))}
                     </TableHead>
                     <TableBody>
-                        <TableRow>
-                            <TableCell>basyx-reader-serialization-two</TableCell>
-                            <TableCell>READ</TableCell>
-                            <TableCell>aas-environment</TableCell>
-                            <TableCell>
-                                https://aas2.uni-h.de/lni0729, https://aas2.uni-h.de/lni0729,
-                                https://aas2.uni-h.de/lni0729, ...
-                            </TableCell>
-                            <TableCell>submodel1, submodel2, submodel3, submodel4, submodel5, submodel6, ...</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>basyx-reader-serialization</TableCell>
-                            <TableCell>CREATE, UPDATE</TableCell>
-                            <TableCell>aas-repo</TableCell>
-                            <TableCell>https://aas2.uni-h.de/lni0729, https://aas2.uni-h.de/lni0729, ...</TableCell>
-                            <TableCell>submodel1, submodel2, submodel6</TableCell>
-                        </TableRow>
+                        {dummyData.map((entry) => (
+                            <TableRow key={entry.name}>
+                                <TableCell>{entry.name}</TableCell>
+                                <TableCell>{entry.action}</TableCell>
+                                <TableCell>{entry.type}</TableCell>
+                                <TableCell>{entry.aasIds.join(', ')}</TableCell>
+                                <TableCell>{entry.submodelIds.join(', ')}</TableCell>
+                            </TableRow>
+                        ))}
                     </TableBody>
                 </Table>
             </TableContainer>
         </Box>
     );
 };
+// TODO: Batches for Action? Length of AASIDS and SubmodelIDS, how can I view a huge List?
+// Test all List implementation and align styling
