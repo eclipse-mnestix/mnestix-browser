59a1fbab393ad8e90515b4c1d915cf18aa94663f
Nils Rothamel <nils.rothamel@xitaso.com>
2025-03-18 14:27:47 +0100

    Add space between headline and info in SubmodelInfoDialog.tsx

--- src/app/[locale]/viewer/_components/submodel/SubmodelInfoDialog.tsx
+++ src/app/[locale]/viewer/_components/submodel/SubmodelInfoDialog.tsx
@@ -17,10 +17,12 @@ export function SubmodelInfoDialog(props: SubmodelInfoDialogProps) {
         <Dialog open={props.open} onClose={props.onClose}>
             <DialogCloseButton handleClose={props.onClose}/>
             <DialogContent style={{ padding: '40px' }}>
-                <Typography variant="h2" color={'primary'}>
+                <Typography variant="h2" color={'primary'} marginBottom={'1em'}>
                     {props.idShort}
                 </Typography>
-                <DataRow title={t('idLabel')}>{props.id}</DataRow>
+                <DataRow title={t('idLabel')} hasDivider={false}>
+                    {props.id}
+                </DataRow>
             </DialogContent>
         </Dialog>
     );
