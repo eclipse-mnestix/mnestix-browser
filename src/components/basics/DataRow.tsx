dd8d78ae8ef9b8dbbea5ca81a9598f911ecc1cdf
Moritz Hofer <moritz.hofer@xitaso.com>
2025-03-28 17:33:51 +0100

    Add Copy Button

--- src/components/basics/DataRow.tsx
+++ src/components/basics/DataRow.tsx
@@ -1,6 +1,11 @@
-import { Box, Divider, Link, SxProps, Theme, Typography } from '@mui/material';
-import React from 'react';
-import { OpenInNew } from '@mui/icons-material';
+import { Box, Divider, IconButton, Link, SxProps, Theme, Tooltip, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { ContentCopy, OpenInNew } from '@mui/icons-material';
+import { useNotificationSpawner } from 'lib/hooks/UseNotificationSpawner';
+import { useIntl } from 'react-intl';
+import { messages } from 'lib/i18n/localization';
+import { CopyButton } from './CopyButton';
+import { useIsMobile } from 'lib/hooks/UseBreakpoints';
 
 type DataRowProps = {
     readonly title?: string | null;
@@ -9,9 +14,34 @@ type DataRowProps = {
     readonly hasDivider?: boolean;
     readonly isLink?: boolean;
     readonly sx?: SxProps<Theme>;
+    readonly testId?: string;
+    readonly withBase64?: boolean;
 };
 
 export function DataRow(props: DataRowProps) {
+    const [isHovered, setIsHovered] = useState(false);
+    const isMobile = useIsMobile();
+    const renderCopyButtons = () => {
+        if (!props.value || isMobile) return null;
+        return (
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <CopyButton 
+                value={props.value} 
+                isVisible={isHovered}
+                data-testid={props.testId || "copy-datarow-value"}
+            />
+            {props.withBase64 && (
+                <CopyButton 
+                value={props.value}
+                isVisible={isHovered}
+                withBase64={true}
+                data-testid={props.testId + "-b64" || "copy-datarow-value-secondary"}
+                />
+            )}
+            </Box>
+        );
+    };
+
     return (
         <Box
             data-testid="data-row"
@@ -24,19 +54,27 @@ export function DataRow(props: DataRowProps) {
                 </Typography>
             )}
             {props.value && (
-                <Typography
-                    style={{ overflowWrap: 'break-word', wordBreak: 'break-word', display: 'inline-block' }}
-                    data-testid="data-row-value"
+                <Box 
+                    display="flex" 
+                    alignItems="center"
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
                 >
-                    {props.isLink ? (
-                        <Link component="a" href={props.value} target="_blank" rel="noopener noreferrer">
-                            {props.value}
-                            <OpenInNew fontSize="small" sx={{ verticalAlign: 'middle', ml: 1 }} />
-                        </Link>
-                    ) : (
-                        props.value
-                    )}
-                </Typography>
+                    <Typography
+                        style={{ overflowWrap: 'break-word', wordBreak: 'break-word', display: 'inline-block' }}
+                        data-testid="data-row-value"
+                    >
+                        {props.isLink ? (
+                            <Link component="a" href={props.value} target="_blank" rel="noopener noreferrer">
+                                {props.value}
+                                <OpenInNew fontSize="small" sx={{ verticalAlign: 'middle', ml: 1 }} />
+                            </Link>
+                        ) : (
+                            props.value
+                        )}
+                    </Typography>
+                    {renderCopyButtons()}
+                </Box>
             )}
             <Box style={{ overflowWrap: 'break-word', wordBreak: 'break-word', display: 'inline-block' }}>
                 {props.children}
