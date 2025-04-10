5a1c891f06548a588086a62a93b9d64fd73618ac
Nils Rothamel <nils.rothamel@xitaso.com>
2025-04-10 17:06:54 +0200

    fix(tests): update test description for DummyComponent to reflect Next Intl

--- src/test-utils/DummyComponentNextIntl.spec.tsx
+++ src/test-utils/DummyComponentNextIntl.spec.tsx
@@ -4,7 +4,7 @@ import { CustomRenderNextIntl } from 'test-utils/CustomRenderNextIntl';
 import { DummyComponentNextIntl } from 'test-utils/DummyComponentNextIntl';
 
 describe('DummyComponent', () => {
-    it('renders the DummyComponent React Intl', async () => {
+    it('renders the DummyComponent Next Intl', async () => {
         CustomRenderNextIntl(<DummyComponentNextIntl />);
         const text = screen.getByTestId('test-text');
         expect(text).toBeDefined();
