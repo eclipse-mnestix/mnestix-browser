83af00cd1654acf66b32db8cb23d9a291c24e9fa
Alina Geesen-Stucky <alina.geesen@xitaso.com>
2025-04-25 13:53:00 +0200

    feat(image-preview-dialog): remove comments for self explanatory code

--- src/app/[locale]/viewer/_components/submodel-elements/generic-elements/ImagePreviewDialog.tsx
+++ src/app/[locale]/viewer/_components/submodel-elements/generic-elements/ImagePreviewDialog.tsx
@@ -4,29 +4,11 @@ import { File } from '@aas-core-works/aas-core3.0-typescript/types';
 import { FileComponent } from 'app/[locale]/viewer/_components/submodel-elements/generic-elements/FileComponent';
 import CloseIcon from '@mui/icons-material/Close';
 
-/**
- * Props for the ImagePreviewDialog component
- */
 interface ImagePreviewDialogProps {
-    /**
-     * Whether the dialog is open
-     */
     open: boolean;
-    /**
-     * Function to handle closing the dialog
-     */
     onClose: () => void;
-    /**
-     * The file to display in the preview
-     */
     file: File | null;
-    /**
-     * The ID of the submodel containing the file
-     */
     submodelId?: string;
-    /**
-     * The path to the submodel element
-     */
     submodelElementPath: string | null;
 }
 
@@ -56,9 +38,9 @@ function ImagePreviewDialog({ open, onClose, file, submodelId, submodelElementPa
                     <CloseIcon />
                 </IconButton>
             </DialogTitle>
-            <DialogContent sx={{ textAlign: 'center', p: 2 }}>
+            <DialogContent sx={{ display: 'flex', justifyContent: 'center', p: 2 }}>
                 {file && submodelElementPath && (
-                    <Box sx={{ maxWidth: '100%', maxHeight: '80vh', minHeight: '100px' }}>
+                    <Box sx={{ maxWidth: '100%', maxHeight: '80vh', minHeight: '200px' }}>
                         <FileComponent
                             file={file}
                             submodelId={submodelId}
