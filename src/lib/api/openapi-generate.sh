62fdc99a71f14bd8e75ed290f2e603779e2e8425
Niklas Sirch-XITASO <niklas.sirch@xitaso.com>
2025-05-15 14:59:11 +0200

    add generator script

--- src/lib/api/openapi-generate.sh
+++ src/lib/api/openapi-generate.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+set -e
+
 ../../../node_modules/.bin/openapi-generator-cli generate
+
+../../../node_modules/.bin/prettier --write aas/**/*.ts
 
 for patch in $(ls patches/*.patch | sort); do
   echo "Applying patch $patch"
