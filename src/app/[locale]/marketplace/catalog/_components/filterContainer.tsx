a6c39a68ad68132e9c62d61913fde2cc3b44c4d0
melanie.gail <melanie.gail@xitaso.com>
2025-05-26 14:53:46 +0200

    handle selection and deselection of children and parents

--- src/app/[locale]/marketplace/catalog/_components/filterContainer.tsx
+++ src/app/[locale]/marketplace/catalog/_components/filterContainer.tsx
@@ -1,45 +1,73 @@
 import { Box, Checkbox, Typography } from '@mui/material';
 import { useTranslations } from 'next-intl';
 import { SimpleTreeView, TreeItem } from '@mui/x-tree-view';
+import { useState } from 'react';
+
+interface ProductDesignation {
+    name: string;
+    value: boolean;
+}
+
+interface ProductFamily {
+    name: string;
+    value: boolean;
+    ProductDesignations: ProductDesignation[];
+}
+
+interface ProductRoot {
+    name: string;
+    value: boolean;
+    ProductFamilies: ProductFamily[];
+}
+
+interface ProductCategory {
+    ProductRoot: ProductRoot;
+}
 
 export function FilterContainer() {
     const t = useTranslations('pages.catalog');
 
     const productCategoryFilters = [
         {
-            ProductRoot: 'Coroflex High-Voltage Cable',
-            ProductFamilies: [
-                {
-                    ProductFamily: 'Shielded Single-Core Cable',
-                    ProductDesignations: [
-                        { ProductDesignation: 'Coroflex High-Voltage Cable 1x120mm²' },
-                        { ProductDesignation: 'Coroflex High-Voltage Cable 1x240mm²' },
-                        { ProductDesignation: 'Coroflex High-Voltage Cable 1x400mm²' },
-                        { ProductDesignation: 'Coroflex High-Voltage Cable 1x630mm²' },
-                        { ProductDesignation: 'Coroflex High-Voltage Cable 1x800mm²' },
-                        { ProductDesignation: 'Coroflex High-Voltage Cable 1x1000mm²' },
-                    ],
-                },
-                {
-                    ProductFamily: 'Shielded Multi-Core Cable', // add checkbox for root and family
-                    ProductDesignations: [
-                        {
-                            ProductDesignation: 'Coroflex High-Voltage Cable 3x120mm²',
-                        },
-                    ],
-                },
-            ],
+            ProductRoot: {
+                name: 'Coroflex High-Voltage Cable',
+                value: false,
+                ProductFamilies: [
+                    {
+                        name: 'Shielded Single-Core Cable',
+                        value: false,
+                        ProductDesignations: [
+                            { name: 'Coroflex High-Voltage Cable 1x120mm²', value: false },
+                            { name: 'Coroflex High-Voltage Cable 1x240mm²', value: false },
+                            { name: 'Coroflex High-Voltage Cable 1x400mm²', value: false },
+                            { name: 'Coroflex High-Voltage Cable 1x630mm²', value: false },
+                            { name: 'Coroflex High-Voltage Cable 1x800mm²', value: false },
+                            { name: 'Coroflex High-Voltage Cable 1x1000mm²', value: false },
+                        ],
+                    },
+                    {
+                        name: 'Shielded Multi-Core Cable',
+                        value: false,
+                        ProductDesignations: [{ name: 'Coroflex High-Voltage Cable 3x120mm²', value: false }],
+                    },
+                ],
+            },
         },
         {
-            ProductRoot: 'CONNECT_TERMINALS',
-            ProductFamilies: [
-                {
-                    ProductFamily: 'DLK 1,2',
-                    ProductDesignations: [{ ProductDesignation: 'DLK 1,2' }],
-                },
-            ],
+            ProductRoot: {
+                name: 'CONNECT_TERMINALS',
+                value: false,
+                ProductFamilies: [
+                    {
+                        name: 'DLK 1,2',
+                        value: false,
+                        ProductDesignations: [{ name: 'DLK 1,2', value: false }],
+                    },
+                ],
+            },
         },
     ];
+    const [filters, setFilters] = useState<ProductCategory[]>(productCategoryFilters);
 
     const eClassFilters = [
         '27-14-01-01',
@@ -52,6 +80,65 @@ export function FilterContainer() {
         '44-05-01-91',
     ];
     const VECFilters = ['PrimaryPartType_Wire', 'PrimaryPartType_PluggableTerminal'];
+
+    function updateCheckboxState(node: ProductRoot | ProductFamily | ProductDesignation, isChecked: boolean) {
+        setFilters((prevFilters) => {
+            return prevFilters.map((category) => {
+                const isRootNode = category.ProductRoot.name === node.name;
+                const updatedRoot = { ...category.ProductRoot };
+
+                const updatedFamilies = updatedRoot.ProductFamilies.map((family) => {
+                    const isFamilyNode = family.name === node.name;
+
+                    if (isRootNode) {
+                        return {
+                            ...family,
+                            value: isChecked,
+                            ProductDesignations: family.ProductDesignations.map((designation) => ({
+                                ...designation,
+                                value: isChecked,
+                            })),
+                        };
+                    }
+
+                    if (isFamilyNode) {
+                        return {
+                            ...family,
+                            value: isChecked,
+                            ProductDesignations: family.ProductDesignations.map((designation) => ({
+                                ...designation,
+                                value: isChecked,
+                            })),
+                        };
+                    }
+
+                    // Behandlung von ProductDesignation-Änderungen
+                    const updatedDesignations = family.ProductDesignations.map((designation) => ({
+                        ...designation,
+                        value: designation.name === node.name ? isChecked : designation.value,
+                    }));
+
+                    const updatedFamily = {
+                        ...family,
+                        value: updatedDesignations.every((d) => d.value),
+                        ProductDesignations: updatedDesignations,
+                    };
+
+                    return updatedFamily;
+                });
+
+                // Update Root basierend auf Families
+                updatedRoot.value = updatedFamilies.every((family) => family.value);
+
+                return {
+                    ProductRoot: {
+                        ...updatedRoot,
+                        ProductFamilies: updatedFamilies,
+                    },
+                };
+            });
+        });
+    }
 
     /**
      * Creates a hierarchy of ECLASS filters for the tree view for the first two numbers.
@@ -147,40 +234,60 @@ export function FilterContainer() {
                         </Typography>
                     }
                 >
-                    {productCategoryFilters.map((productCategory) => {
+                    {filters.map((productCategory) => {
                         return (
                             <TreeItem
-                                key={productCategory.ProductRoot}
-                                itemId={productCategory.ProductRoot}
+                                key={productCategory.ProductRoot.name}
+                                itemId={productCategory.ProductRoot.name}
                                 label={
                                     <Box display="flex" alignItems="center">
-                                        <Checkbox onClick={(event) => event.stopPropagation()} />
-                                        {productCategory.ProductRoot}
+                                        <Checkbox
+                                            onClick={(event) => event.stopPropagation()}
+                                            checked={productCategory.ProductRoot.value}
+                                            onChange={(event) =>
+                                                updateCheckboxState(productCategory.ProductRoot, event.target.checked)
+                                            }
+                                        />
+                                        {productCategory.ProductRoot.name}
                                     </Box>
                                 }
                             >
-                                {productCategory.ProductFamilies.map((productFamily) => {
+                                {productCategory.ProductRoot.ProductFamilies.map((productFamily) => {
                                     return (
                                         <TreeItem
-                                            key={productFamily.ProductFamily}
-                                            itemId={productFamily.ProductFamily}
+                                            key={productFamily.name}
+                                            itemId={productFamily.name}
                                             label={
                                                 <Box display="flex" alignItems="center">
-                                                    <Checkbox onClick={(event) => event.stopPropagation()} />
-                                                    {productFamily.ProductFamily}
+                                                    <Checkbox
+                                                        onClick={(event) => event.stopPropagation()}
+                                                        checked={productFamily.value}
+                                                        onChange={(event) =>
+                                                            updateCheckboxState(productFamily, event.target.checked)
+                                                        }
+                                                    />
+                                                    {productFamily.name}
                                                 </Box>
                                             }
                                         >
                                             {productFamily.ProductDesignations.map((productDesignation) => {
                                                 return (
                                                     <Box
-                                                        key={productDesignation.ProductDesignation}
+                                                        key={productDesignation.name}
                                                         display="flex"
                                                         alignItems="center"
                                                         ml={4}
                                                     >
-                                                        <Checkbox />
-                                                        <Typography>{productDesignation.ProductDesignation}</Typography>
+                                                        <Checkbox
+                                                            checked={productDesignation.value}
+                                                            onChange={(event) =>
+                                                                updateCheckboxState(
+                                                                    productDesignation,
+                                                                    event.target.checked,
+                                                                )
+                                                            }
+                                                        />
+                                                        <Typography>{productDesignation.name}</Typography>
                                                     </Box>
                                                 );
                                             })}
