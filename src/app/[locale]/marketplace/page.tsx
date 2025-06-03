16aaf1468a91ef59380fa1af2aad6cb1e3621fe6
Nils Rothamel <nils.rothamel@xitaso.com>
2025-06-03 16:20:57 +0200

    feat(marketplace): add search bar

--- src/app/[locale]/marketplace/page.tsx
+++ src/app/[locale]/marketplace/page.tsx
@@ -58,18 +58,7 @@ export default function Page() {
                 <Box marginBottom="1em">
                     <Breadcrumbs links={breadcrumbLinks} />
                 </Box>
-                <Box display="flex" alignItems="center" justifyContent="space-between" mb={2}>
-                    <ListHeader header={t('marketplaceTitle')} subHeader={t('marketplaceSubtitle')} />
-                    <Button
-                        variant="contained"
-                        startIcon={<InsightsIcon />}
-                        onClick={() => setIsConstructionDialogOpen(true)}
-                        sx={{ whiteSpace: 'nowrap', ml: 2 }}
-                        aria-label="Chatbot"
-                    >
-                        Chatbot
-                    </Button>
-                </Box>
+                <ListHeader header={t('marketplaceTitle')} subHeader={t('marketplaceSubtitle')} />
                 <Box display="flex" alignItems="center" marginTop="1.5rem" marginBottom="1.5rem">
                     <ContentPasteSearchIcon fontSize="large" color="primary" sx={{ mr: 1 }} />
                     <Typography variant="h4" fontWeight={600} component="h2">
@@ -173,6 +162,15 @@ export default function Page() {
                                 </Box>
                             </Card>
                         ))}
+                        <Button
+                        variant="contained"
+                        startIcon={<InsightsIcon />}
+                        onClick={() => setIsConstructionDialogOpen(true)}
+                        sx={{ position: 'fixed', bottom: '10%', right: '5%' }}
+                        aria-label="Chatbot"
+                        >
+                        Chatbot
+                        </Button>
                     </Box>
                 )}
             </Box>
