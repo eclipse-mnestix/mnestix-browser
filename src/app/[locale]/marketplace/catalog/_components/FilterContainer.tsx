04cbdad1a9394bc0ef670e912a34b9bd716eab5f
Melanie Gail <melanie.gail@xitaso.com>
2025-06-03 14:33:16 +0200

    feat(add graphql) catalog filter (#278)
    * install apollo
    
    * first graphql client
    
    * only show results from searcher
    
    * added manufacturer name
    
    * eclass filter
    
    * eclass filter
    
    * eclass filter
    
    * vec filter
    
    * vec filter
    
    * error handling
    
    * product root
    
    * warning when no searcher available
    
    * feat(filter options): Add fetch filter options from API (#281)
    
    * get filter values from api
    
    * rename variable
    
    * checkbox state update fixed
    
    * Filter product classification
    
    * filtering updated
    
    * replace localhost url
    
    * Add translations for eclass
    
    ---------
    
    Co-authored-by: melanie.gail <melanie.gail@xitaso.com>
    
    ---------
    
    Co-authored-by: Pawel Baran <98280057+pawel-baran-se@users.noreply.github.com>

--- src/app/[locale]/marketplace/catalog/_components/FilterContainer.tsx
+++ src/app/[locale]/marketplace/catalog/_components/FilterContainer.tsx
@@ -1,117 +1,197 @@
-import { Box, Checkbox, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import { useTranslations } from 'next-intl';
-import { SimpleTreeView, TreeItem } from '@mui/x-tree-view';
 import { EClassFilter } from 'app/[locale]/marketplace/catalog/_components/EClassFilter';
 import { ProductCategoryFilter } from 'app/[locale]/marketplace/catalog/_components/ProductCategoryFilter';
+import { useState } from 'react';
+import { VecFilter } from 'app/[locale]/marketplace/catalog/_components/VecFilter';
+import { useAsyncEffect } from 'lib/hooks/UseAsyncEffect';
+import { CenteredLoadingSpinner } from 'components/basics/CenteredLoadingSpinner';
+import { searchProducts } from 'lib/api/graphql/catalogActions';
+import { useShowError } from 'lib/hooks/UseShowError';
+import { useParams } from 'next/navigation';
 
 export interface FilterQuery {
     key: string;
-    value: boolean;
+    value: string;
 }
 
-export function FilterContainer() {
-    const t = useTranslations('pages.catalog');
+type ProductDesignation = {
+    name: string;
+};
 
-    // This dummy filters needs to be replaced with actual data fetching logic
-    const productCategoryFilters = [
-        {
+type ProductFamily = {
+    name: string;
+    ProductDesignations: ProductDesignation[];
+};
+
+type ProductRoot = {
+    name: string;
+    ProductFamilies: ProductFamily[];
+};
+
+type FilterOptions = {
+    ECLASS: Set<string>;
+    VEC: Set<string>;
+    PRODUCT_ROOT: ProductRoot[];
+};
+
+export function FilterContainer(props: { onFilterChanged(query: FilterQuery[]): void }) {
+    const t = useTranslations('pages.catalog');
+    const [activeFilters, setActiveFilters] = useState<FilterQuery[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [filterOptions, setFilterOptions] = useState<FilterOptions>();
+    const { showError } = useShowError();
+    const params = useParams();
+    const locale = params?.locale as string;
+
+    useAsyncEffect(async () => {
+        setLoading(true);
+        const results = await searchProducts();
+
+        if (results.isSuccess) {
+            const products = results.result;
+            const eclassSet = new Set<string>();
+            const vecSet = new Set<string>();
+            const productRoot: ProductRoot[] = [];
+
+            products.forEach((product) => {
+                product.productClassifications.forEach((classification) => {
+                    if (classification.system === 'ECLASS' && classification.productId) {
+                        eclassSet.add(classification.productId);
+                    }
+                    if (classification.system === 'VEC' && classification.productId) {
+                        vecSet.add(classification.productId);
+                    }
+                });
+            });
+            products.forEach((product) => {
+                const rootName =
+                    product.productRoot?.mlValues.find((value) => value.language === (locale ?? 'en'))?.text ||
+                    'Unknown Root';
+                let root = productRoot.find((r) => r.name === rootName);
+                if (!root) {
+                    root = { name: rootName, ProductFamilies: [] };
+                    productRoot.push(root);
+                }
+                const familyName =
+                    product.productFamily?.mlValues.find((family) => family.language === (locale ?? 'en'))?.text ||
+                    'Unknown Family';
+                let familyItem = root.ProductFamilies.find((f) => f.name === familyName);
+                if (!familyItem) {
+                    familyItem = { name: familyName, ProductDesignations: [] };
+                    root.ProductFamilies.push(familyItem);
+                }
+                const designationName =
+                    product.productDesignation?.mlValues.find((des) => des.language === (locale ?? 'en'))?.text ||
+                    'Unknown Designation';
+                let desItem = familyItem.ProductDesignations.find((f) => f.name === designationName);
+                if (!desItem) {
+                    desItem = { name: designationName };
+                    familyItem.ProductDesignations.push(desItem);
+                }
+            });
+
+            setFilterOptions({
+                ECLASS: eclassSet,
+                VEC: vecSet,
+                PRODUCT_ROOT: productRoot,
+            });
+            // Initialize active filters with ECLASS and VEC options
+        } else {
+            showError(results.message);
+        }
+        setLoading(false);
+    }, []);
+
+    const productCategoryFilters =
+        filterOptions?.PRODUCT_ROOT?.map((root) => ({
             ProductRoot: {
-                name: 'Coroflex High-Voltage Cable',
-                ProductFamilies: [
-                    {
-                        name: 'Shielded Single-Core Cable',
-                        ProductDesignations: [
-                            { name: 'Coroflex High-Voltage Cable 1x120mm²' },
-                            { name: 'Coroflex High-Voltage Cable 1x240mm²' },
-                            { name: 'Coroflex High-Voltage Cable 1x400mm²' },
-                            { name: 'Coroflex High-Voltage Cable 1x630mm²' },
-                            { name: 'Coroflex High-Voltage Cable 1x800mm²' },
-                            { name: 'Coroflex High-Voltage Cable 1x1000mm²' },
-                        ],
-                    },
-                    {
-                        name: 'Shielded Multi-Core Cable',
-                        ProductDesignations: [{ name: 'Coroflex High-Voltage Cable 3x120mm²' }],
-                    },
-                ],
+                name: root.name,
+                ProductFamilies: root.ProductFamilies.map((family) => ({
+                    name: family.name,
+                    ProductDesignations: family.ProductDesignations.map((designation) => ({
+                        name: designation.name,
+                    })).sort((a, b) => a.name.localeCompare(b.name)),
+                })).sort((a, b) => a.name.localeCompare(b.name)),
             },
-        },
-        {
-            ProductRoot: {
-                name: 'CONNECT_TERMINALS',
-                ProductFamilies: [
-                    {
-                        name: 'DLK 1,2',
-                        ProductDesignations: [{ name: 'DLK 1,2' }],
-                    },
-                ],
-            },
-        },
-    ];
-    const eClassFilters = [
-        '27-14-01-01',
-        '27-14-01-02',
-        '27-14-01-03',
-        '27-14-01-04',
-        '27-14-01-05',
-        '27-14-01-06',
-        '44-04-01-91',
-        '44-05-01-91',
-    ];
-    const VECFilters = ['PrimaryPartType_Wire', 'PrimaryPartType_PluggableTerminal'];
+        })).sort((a, b) => a.ProductRoot.name.localeCompare(b.ProductRoot.name)) ?? [];
+    const eClassFilters = filterOptions?.ECLASS ? Array.from(filterOptions.ECLASS) : [];
+    const VECFilters = filterOptions?.VEC ? Array.from(filterOptions.VEC) : [];
 
     // process productCategoryFilters to match the expected structure
     function addBooleanValues<T extends { name: string }>(item: T): T & { value: boolean } {
         return {
             ...item,
-            value: false
+            value: false,
         };
     }
 
     function transformProductCategories(categories: typeof productCategoryFilters) {
-        return categories.map(category => ({
+        return categories.map((category) => ({
             ProductRoot: {
                 ...addBooleanValues(category.ProductRoot),
-                ProductFamilies: category.ProductRoot.ProductFamilies.map(family => ({
+                ProductFamilies: category.ProductRoot.ProductFamilies.map((family) => ({
                     ...addBooleanValues(family),
-                    ProductDesignations: family.ProductDesignations.map(designation =>
-                        addBooleanValues(designation)
-                    )
-                }))
-            }
+                    ProductDesignations: family.ProductDesignations.map((designation) => addBooleanValues(designation)),
+                })),
+            },
         }));
     }
 
-    function onFilterChanged(query: FilterQuery) {
-        console.log(`Filter changed: ${query.key} is now ${query.value}`);
+    function onFilterChangedByCategory(key: string, values: string[]) {
+        setActiveFilters((prevFilters) => {
+            const filtered = prevFilters.filter((filter) => filter.key !== key);
+
+            const newFilters = values.map((value) => ({ key, value }));
+            return [...filtered, ...newFilters];
+        });
+    }
+
+    function onFilterClassificationChangedCategory(query: FilterQuery[]) {
+        setActiveFilters(query);
+    }
+
+    function applyFilter() {
+        console.log(activeFilters);
+        props.onFilterChanged(activeFilters);
     }
 
     return (
-        <>
-            <Typography variant="h4" fontWeight={600} mb={1}>
-                {t('filter')}
-            </Typography>
-            <EClassFilter eClassFilters={eClassFilters} onFilterChanged={onFilterChanged}/>
-            <SimpleTreeView>
-                <TreeItem
-                    itemId="vec"
-                    label={
-                        <Typography variant="h5" my={1}>
-                            VEC
-                        </Typography>
-                    }
-                >
-                    {VECFilters.map((vec) => {
-                        return (
-                            <Box key={vec} display="flex" alignItems="center">
-                                <Checkbox />
-                                <Typography>{vec.replace('PrimaryPartType_', '')}</Typography>
-                            </Box>
-                        );
-                    })}
-                </TreeItem>
-            </SimpleTreeView>
-            <ProductCategoryFilter productCategoryFilters={transformProductCategories(productCategoryFilters)} onFilterChanged={onFilterChanged}/>
-        </>
+        <Box>
+            {loading ? (
+                <CenteredLoadingSpinner />
+            ) : (
+                <>
+                    <Typography variant="h4" fontWeight={600} mb={1}>
+                        {t('filter')}
+                    </Typography>
+                    <EClassFilter
+                        eClassFilters={eClassFilters}
+                        onFilterChanged={(values) =>
+                            onFilterChangedByCategory(
+                                'ECLASS',
+                                values.map((f) => f.value),
+                            )
+                        }
+                    />
+                    <VecFilter
+                        vecFilters={VECFilters}
+                        onFilterChanged={(values) =>
+                            onFilterChangedByCategory(
+                                'VEC',
+                                values.map((f) => f.value),
+                            )
+                        }
+                    />
+                    <ProductCategoryFilter
+                        productCategoryFilters={transformProductCategories(productCategoryFilters)}
+                        onFilterChanged={(values) => onFilterClassificationChangedCategory(values)}
+                    />
+                    <Button sx={{ mt: 3 }} variant="contained" onClick={applyFilter}>
+                        Apply Filter
+                    </Button>
+                </>
+            )}
+        </Box>
     );
 }
---
+++ src/lib/api/graphql/catalogActions.ts
@@ -0,0 +1,90 @@
+'use server';
+
+import { gql } from '@apollo/client';
+import { FilterQuery } from 'app/[locale]/marketplace/catalog/_components/FilterContainer';
+import { client } from 'lib/api/graphql/apolloClient';
+import { GET_PRODUCTS, searchQuery, SearchResponse, SearchResponseEntry } from 'lib/api/graphql/catalogQueries';
+import { ApiResponseWrapper, wrapErrorCode, wrapSuccess } from 'lib/util/apiResponseWrapper/apiResponseWrapper';
+
+function buildFilterInput(filters?: FilterQuery[]): string {
+    if (!filters || filters.length === 0) {
+        return '';
+    }
+    const eclassFilter = filters.filter((filter) => filter.key === 'ECLASS');
+    const vecFilter = filters.filter((filter) => filter.key === 'VEC');
+    const productClassificationFilters: string[] = [];
+    const filterArray = [];
+
+    // ECLASS Filters
+    if (eclassFilter.length > 0) {
+        productClassificationFilters.push(`{
+            system: { eq: "ECLASS" }
+            productId: { in: [${eclassFilter.map((filter) => `"${filter.value.trim()}"`).join(',')}] }
+        }`);
+    }
+
+    // VEC Filters
+    if (vecFilter.length > 0) {
+        productClassificationFilters.push(`{
+            system: { eq: "VEC" }
+            productId: { in: [${vecFilter.map((filter) => `"${filter.value.trim()}"`).join(',')}] }
+        }`);
+    }
+
+    if (productClassificationFilters.length !== 0) {
+        const productClassificationFilterString = ` {productClassifications: { some: { or: [${productClassificationFilters.join(',')}] } } }`;
+        filterArray.push(productClassificationFilterString);
+    }
+
+    // ProductClassification filters:
+    const productRootFilter = filters.filter((filter) => filter.key === 'PRODUCT_ROOT');
+    if (productRootFilter) {
+        filterArray.push(`
+            {productRoot: { mlValues: {some: { text: { in: [${productRootFilter.map((filter) => `"${filter.value.trim()}"`).join(',')}] }}}}}
+        `);
+    }
+
+    const productFamilyFilter = filters.filter((filter) => filter.key === 'PRODUCT_FAMILY');
+    if (productFamilyFilter) {
+        filterArray.push(`
+            {productFamily: { mlValues: {some: { text: { in: [${productFamilyFilter.map((filter) => `"${filter.value.trim()}"`).join(',')}] }}}}}
+        `);
+    }
+
+    const productDesignationFilter = filters.filter((filter) => filter.key === 'PRODUCT_DESIGNATION');
+    if (productDesignationFilter) {
+        filterArray.push(`
+            {productDesignation: { mlValues: {some: { text: { in: [${productDesignationFilter.map((filter) => `"${filter.value.trim()}"`).join(',')}] }}}}}
+        `);
+    }
+
+    console.log(filterArray);
+
+    if (filterArray.length === 0) {
+        return '';
+    }
+    return filterArray.length > 1 ? `(where: { or: [${filterArray.join(' , ')}]})` : `(where: { ${filterArray} })`;
+}
+
+export async function searchProducts(filters?: FilterQuery[]): Promise<ApiResponseWrapper<SearchResponseEntry[]>> {
+    const queryString = searchQuery(buildFilterInput(filters));
+    console.log(queryString);
+
+    const query = gql(queryString);
+    try {
+        const { data } = await client.query<SearchResponse>({
+            query,
+        });
+        return wrapSuccess(data.entries);
+    } catch (error) {
+        console.error('Error searching products:', JSON.stringify(error.result));
+        return wrapErrorCode('UNKNOWN_ERROR', error);
+    }
+}
+
+export async function getProducts() {
+    const { data } = await client.query<SearchResponse>({
+        query: GET_PRODUCTS,
+    });
+    return data.entries;
+}
