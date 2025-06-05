0e1c4fc9ae6a1d436fb8573e70d678bdcde21037
Pawel Baran <98280057+pawel-baran-se@users.noreply.github.com>
2025-06-05 07:49:04 +0200

    (database): Add aasSearcher and image column to MnestixConnections table (#282)
    * (database): Add aasSearcher and image column to MnestixConnections table
    
    * feat(database): add manufacturerName column to MnestixConnection model and update related logic
    
    * renamed name column
    
    ---------
    
    Co-authored-by: Nils Rothamel <nils.rothamel@xitaso.com>
    Co-authored-by: melanie.gail <melanie.gail@xitaso.com>

---
+++ prisma/migrations/20250603111047_add_aas_searcher_and_image_columns/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "MnestixConnection" ADD COLUMN "aasSearcher" TEXT;
+ALTER TABLE "MnestixConnection" ADD COLUMN "image" TEXT;
+ALTER TABLE "MnestixConnection" ADD COLUMN "name" TEXT;
