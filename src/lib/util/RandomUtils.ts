8db819f01d2b07eaa45dc092858f7f4e65e23610
Pawel Baran <pawel.baran@xitaso.com>
2025-06-19 10:34:32 +0200

    Remove unnecessary comment

--- src/lib/util/RandomUtils.ts
+++ src/lib/util/RandomUtils.ts
@@ -7,7 +7,7 @@ export function generateRandomId(): string {
 export function formatTimestamp(input: string): string {
     const date = new Date(input);
     const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+    const month = String(date.getMonth() + 1).padStart(2, '0');
     const year = String(date.getFullYear());
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
