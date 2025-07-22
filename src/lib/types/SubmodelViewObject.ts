c61dbab2fedf7d77034fbdb82854ea376e397aa9
JonathanXITASO <jonathan.sigrist@xitaso.com>
2025-07-22 14:29:51 +0200

    SubmodelViewObject uses both types

--- src/lib/types/SubmodelViewObject.ts
+++ src/lib/types/SubmodelViewObject.ts
@@ -1,9 +1,10 @@
 import { Submodel, SubmodelElementChoice } from 'lib/api/aas/models';
+import * as WorksTypes from '@aas-core-works/aas-core3.0-typescript/types';
 
 export interface SubmodelViewObject {
     id: string;
     name: string;
-    data?: Submodel | SubmodelElementChoice;
+    data?: Submodel | SubmodelElementChoice | WorksTypes.Submodel | WorksTypes.ISubmodelElement;
     children: SubmodelViewObject[];
     hasValue?: boolean;
     isAboutToBeDeleted?: boolean;
