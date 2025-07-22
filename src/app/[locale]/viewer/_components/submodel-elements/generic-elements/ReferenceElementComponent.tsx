1a366d1398834aec9c3cb441b41cadc544511125
Pawel Baran <98280057+pawel-baran-se@users.noreply.github.com>
2025-07-22 11:43:40 +0200

    Update src/app/[locale]/viewer/_components/submodel-elements/generic-elements/ReferenceElementComponent.tsx
    Apply review
    
    Co-authored-by: Jonathan Sigrist <jonathan.sigrist@xitaso.com>
    Signed-off-by: Pawel Baran <98280057+pawel-baran-se@users.noreply.github.com>

--- src/app/[locale]/viewer/_components/submodel-elements/generic-elements/ReferenceElementComponent.tsx
+++ src/app/[locale]/viewer/_components/submodel-elements/generic-elements/ReferenceElementComponent.tsx
@@ -11,7 +11,7 @@ export function ReferenceElementComponent(props: ReferenceElementComponentProps)
     const t = useTranslations('components.referenceElementComponent');
 
     if (!props.refElement.value || props.refElement.value === undefined) {
-        return <Typography>{t('noValueSpecified')}</Typography>;
+        return <Typography variant="body2">{t('noValueSpecified')}</Typography>;
     }
 
     return <ReferenceComponent reference={props.refElement.value} />;
