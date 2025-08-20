ceeac5572926ffc6ef29574fc43e4c189dae2980
Moritz Hofer <moritz.hofer@xitaso.com>
2025-08-20 14:40:09 +0200

    Use native Next JS redirect

--- src/app/[locale]/compare/page.tsx
+++ src/app/[locale]/compare/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { CompareAasContextProvider } from 'components/contexts/CompareAasContext';
-import { Box } from '@mui/material';
-import { CompareView } from './_components/CompareView';
+import { redirect } from 'next/navigation';
+// import { CompareAasContextProvider } from 'components/contexts/CompareAasContext';
+// import { Box } from '@mui/material';
+// import { CompareView } from './_components/CompareView';
 
 /**
  * Comparison View
@@ -11,30 +12,22 @@ import { CompareView } from './_components/CompareView';
  * @constructor
  */
 export default function page() {
-    const pageStyles = {
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '30px',
-        width: '100%',
-        marginBottom: '50px',
-        marginTop: '20px',
-    };
+    // const pageStyles = {
+    //     display: 'flex',
+    //     flexDirection: 'column',
+    //     gap: '30px',
+    //     width: '100%',
+    //     marginBottom: '50px',
+    //     marginTop: '20px',
+    // };
     // Disables the comparison feature for now as it is broken
     // TODO MNE-299: Make Comparison Feature work again
-    if (typeof window !== 'undefined') {
-        const segments = window.location.pathname.split('/').filter(Boolean);
-        const locale = segments[0] ?? '';
-        const homepage = locale ? `/${locale}` : '/';
-        if (window.location.pathname !== homepage) {
-            window.location.replace(homepage);
-        }
-        return null;
-    }
-    return (
-        <CompareAasContextProvider>
-            <Box sx={pageStyles}>
-                <CompareView></CompareView>
-            </Box>
-        </CompareAasContextProvider>
-    );
+    redirect('/'); // Redirect to home page for now
+    // return (
+    //     <CompareAasContextProvider>
+    //         <Box sx={pageStyles}>
+    //             <CompareView></CompareView>
+    //         </Box>
+    //     </CompareAasContextProvider>
+    // );
 }
