ac281f825024436400d5b20286848e85fd2c12c7
Moritz Hofer <84278634+hofermo@users.noreply.github.com>
2025-08-29 14:37:25 +0200

    refactor(concept-description-service): Cleanup Concept Description Service (#442)
    * Update Concept Description to fit better to other services
    
    * Add CoPilot Comments & a Search everywhere function
    
    * Add comments

--- src/lib/services/concept-description-service/conceptDescriptionRepositoryActions.ts
+++ src/lib/services/concept-description-service/conceptDescriptionRepositoryActions.ts
@@ -1,18 +1,27 @@
 'use server';
 
-import { ApiResponseWrapper } from 'lib/util/apiResponseWrapper/apiResponseWrapper';
+import { ApiResponseWrapper, wrapErrorCode } from 'lib/util/apiResponseWrapper/apiResponseWrapper';
 import { ConceptDescription } from 'lib/api/aas/models';
 import { ConceptDescriptionRepositoryService } from './ConceptDescriptionRepositoryService';
 import { createRequestLogger, logInfo } from 'lib/util/Logger';
 import { headers } from 'next/headers';
+import { ApiResultStatus } from 'lib/util/apiResponseWrapper/apiResultStatus';
 
-
-export async function getConceptDescriptionById(conceptDescriptionId: string, infrastructureName: string | undefined = undefined): Promise<ApiResponseWrapper<ConceptDescription>> {
+export async function getConceptDescriptionById(
+    conceptDescriptionId: string,
+    infrastructureName: string | undefined = undefined,
+): Promise<ApiResponseWrapper<ConceptDescription>> {
     const logger = createRequestLogger(await headers());
     logInfo(logger, getConceptDescriptionById.name, 'Requested Concept Description', {
         conceptDescriptionId: conceptDescriptionId,
         infrastructureName: infrastructureName,
     });
+    if (infrastructureName == undefined) {
+        return wrapErrorCode(
+            ApiResultStatus.BAD_REQUEST,
+            `No infrastructure name provided to fetch ${conceptDescriptionId} from.`,
+        );
+    }
     const service = ConceptDescriptionRepositoryService.create();
-    return service.getConceptDescriptionByIdFromRepositories(conceptDescriptionId, infrastructureName);
+    return service.getConceptDescriptionByIdFromInfrastructure(conceptDescriptionId, infrastructureName);
 }
