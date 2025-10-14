5369a74688e001e9fb24339f8c204d88a1f2bf92
melanie.gail <melanie.gail@xitaso.com>
2025-10-14 07:48:28 +0200

    fix lint issues

--- src/lib/services/templateApiWithAuthActions.ts
+++ src/lib/services/templateApiWithAuthActions.ts
@@ -1,27 +1,27 @@
 'use server';
 
-import { mnestixFetch, mnestixFetchRaw } from 'lib/api/infrastructure';
+import { mnestixFetch } from 'lib/api/infrastructure';
 import { TemplateClient } from 'lib/api/generated-api/clients.g';
 import { Submodel } from 'lib/api/aas/models';
 import EmptyDefaultTemplate from 'assets/submodels/defaultEmptySubmodel.json';
 import { envs } from 'lib/env/MnestixEnv';
 import { getDefaultInfrastructure } from './database/infrastructureDatabaseActions';
 import { createSecurityHeaders } from 'lib/util/securityHelpers/SecurityConfiguration';
-import { Configuration } from 'lib/api/mnestix-aas-generator/v2/runtime';
-import { BlueprintsApi } from 'lib/api/mnestix-aas-generator/v2/apis/BlueprintsApi';
+//import { Configuration } from 'lib/api/mnestix-aas-generator/v2/runtime';
 
 export async function createBlueprint(template: Submodel | typeof EmptyDefaultTemplate): Promise<string> {
     const defaultInfrastructure = await getDefaultInfrastructure();
     const securityHeaders = await createSecurityHeaders(defaultInfrastructure);
-    const fetchClient = mnestixFetchRaw(securityHeaders);
-
+/*    TODO MNE-371
+const fetchClient = mnestixFetchRaw(securityHeaders);
+ 
     const apiConfig = new Configuration({
         basePath: envs.MNESTIX_AAS_GENERATOR_API_URL,
         fetchApi: (input: RequestInfo | URL, init?: RequestInit) => fetchClient.fetch(input, init),
     });
 
     // TODO if v2:
-/*     const blueprintApiClient = new BlueprintsApi(apiConfig);
+    const blueprintApiClient = new BlueprintsApi(apiConfig);
     return blueprintApiClient.blueprintsCreateBlueprint({
         apiVersion: 'v2',
         body: template,
