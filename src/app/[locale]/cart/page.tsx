b0168e2caaba1c5b3f0dcb02853c2ef35874d462
Gregor <contact@gregors-computer-lab.com>
2025-11-14 10:36:13 +0100

    feat(cart view): init cart view
    very basic cart view with heading and subheading

---
+++ src/app/[locale]/cart/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { Box } from '@mui/material';
+import ListHeader from 'components/basics/ListHeader';
+import { useTranslations } from 'next-intl';
+
+export default function Page() {
+    const t = useTranslations('pages.cart');
+    return (
+        <Box display="flex" flexDirection="column" marginTop="0px" marginBottom="50px" width="100%">
+            <Box width="90%" margin="auto">
+                <Box marginTop="2rem" marginBottom="2.25rem">
+                    <ListHeader header={t('header')} subHeader={t('subHeader')} />
+                </Box>
+            </Box>
+        </Box>
+    );
+}
