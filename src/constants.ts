06c0d093ac66fdeb7b7a27b38709641b52dd51a8
artacho-xitaso <jose.artacho@xitaso.com>
2026-01-20 12:28:50 +0100

    eslint react hooks downgrade to previous version (5.2.0)

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-import path from "path";
+import path from 'path';
 
 const dbPath = path.join(process.cwd(), 'prisma', 'database', 'mnestix-database.db');
 export const DATABASE_URL = `file:${dbPath}`;
